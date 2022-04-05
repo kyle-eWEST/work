b300ec9caa004e1abe0ce9a229153d8e5f1a8808
kyle-eWEST <64759241+kyle-eWEST@users.noreply.github.com>
2022-04-05 15:17:33 +0100

    Add files via upload

---
+++ kylewest.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+  
+
+
+
+  var typed = new Typed(".auto-inner", {
+    strings: ["A WEB DEVELOPER", "I'M A WEB DESIGNER", "A GRAPHICS DESIGNER"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+  });
+});
